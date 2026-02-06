3982ec25f430629a677fda8a2e580d52ca3c5c02
suhaina0143-dot <suhaina0143@gmail.com>
2026-02-06 16:43:12 +0530

    file added

---
+++ Run time.py
@@ -0,0 +1,7 @@
+m1=int(input("Enter mark1:"))
+m2=int(input("Enter mark2:"))
+m3=int(input("Enter mark3:"))
+m4=int(input("Enter mark4:"))
+m5=int(input("Enter mark5:"))
+total=m1+m2+m3+m4+m5
+print("total mark:",total)
